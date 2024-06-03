3c15ffa1209cbf8014848e997b24354404c8a60f
Evgeniy188 <80258394+Evgeniy188@users.noreply.github.com>
2024-06-03 13:00:57 +0000

    First attempt

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,7 +1,5 @@
 """Django settings for yatube project."""
 
-from datetime import timedelta
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,13 +101,13 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ]
-} 
+}
 
 # Static files (CSS, JavaScript, Images)
 
