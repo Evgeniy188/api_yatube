3fb9e1deba10a25ac2b0c6c49dceceb79132342f
Evgeniy188 <80258394+Evgeniy188@users.noreply.github.com>
2024-06-03 13:49:09 +0000

    First attempt

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -12,7 +12,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id','text', 'pub_date', 'author', 'image', 'group')
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
 
 
 class CommentSerializer(serializers.ModelSerializer):
